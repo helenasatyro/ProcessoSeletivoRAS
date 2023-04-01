4b3a99eb6361b59083726eebfa1f8019505db27c
Helena Sátyro <mhsatyro@gmail.com>
2023-04-01 11:28:37 -0300

    MissaoOpenCV

---
+++ MissaoOpenCV/capituloUm.py
@@ -0,0 +1,16 @@
+# Importa openCV
+import cv2
+# Lê a imagem, dados guaradados em lista de atributos
+imagem = cv2.imread('fotnha.jpg')
+print(imagem.shape)
+print('Largura em pixels: ', end='')
+print(imagem.shape[1]) #largura da imagem
+print('Altura em pixels: ', end='')
+print(imagem.shape[0]) #altura da imagem
+print('Qtde de canais: ', end='')
+print(imagem.shape[2])
+# Mostra a imagem com a função imshow
+cv2.imshow("Nome da janela", imagem)
+cv2.waitKey(0) #espera pressionar qualquer tecla
+# Salvar a imagem no disco com função imwrite()
+cv2.imwrite("saida.jpg", imagem)
