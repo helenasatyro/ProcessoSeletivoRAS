0c4693e5f020a1cf366e7582b6864d061a4b13f5
Helena Sátyro <mhsatyro@gmail.com>
2023-04-13 20:26:53 -0300

    segunda etapa

--- SegundaEtapa/RelatorioSegundaEtapa.md
+++ SegundaEtapa/RelatorioSegundaEtapa.md
@@ -11,21 +11,19 @@ A forma de condução deste trabalho está descrita em três partes:
 2. Descrição do código: em anexo, o código usado no programa está documentado descrevendo cada ação.
 
 ## 1. Desenvolvimento
-### A. Calibrar a detecção de cores
+### A. Calibrar a detecção de cores - Estudo
+Para compreender completamente cada parte do funcionamento do código e da biblioteca, diversas implementações foram testadas, o histórico de código e comentários de cada uma está descrito no relatório ou no próprio código.
+
+Para compreender a detecção pura de cor: calibrando detecção de azul.
 A partir do mapa de HSV em anexo (M), foram escolhidos valores iniciais para a calibragem: 
 > H - 110 a 130, S - 120 a 255, V - 20 a 255
 
 Foi necessário ajustar os valores para que ignorasse por exemplo o azulado de paredes brancas ou pretos azulados, que criavam ruído na identificação de objetos bem definidos, além de contornos gerados por ruído na imagem. (Anexos Aa 1-2)
 
-O range obtido e mantido para a detecção de cores em código é de um azul de tom médio a escuro, foram usados como base os valores na apostila[1] disponibilizada, com alguns ajustes. 
+O range obtido é de um azul de tom médio a escuro, foram usados como base os valores na apostila[1] disponibilizada, com alguns ajustes. 
 > H - 90 a 255, S - 0 a 90, V - 0 a 255
 
-Observando a máscara criada com a função inRange, pequenos incrementos e decrementos foram feitos até atingir um nível satisfatório de precisão. (Anexos A 1-4)
-
-Foram seguidos os mesmos passos para as demais cores. Valores finais:
-> Amarelo:
-
-> Vermelho: 
+Observando a máscara criada com a função inRange, pequenos incrementos e decrementos foram feitos até atingir um nível satisfatório de precisão. (Anexos A 1-4) Note, no arquivo parcialB.py, linhas 10 e 11, os valores estão definidos em um array numpy.
 
 ### B. Desenhar os contornos dos objetos
 A partir do vídeo[2] disponibilizado, foi possível desenhar retângulos sobre os objetos de forma rudimentar, desconsiderando a rotação e as bordas reais. (Anexos B 1-5)
@@ -52,10 +50,37 @@ Na apostila, é adicionada uma linha de código que faz com que apenas o maior o
 É válido mencionar que caso fosse preciso contornar cada objeto de acordo com as bordas (da cor) reais, seria pertinente usar o parâmetro CHAIN_APPROX_NONE no momento da busca pelo contorno, e o próprio objeto contorno retornado como parâmetro em drawContours ao invés do retângulo. 
 
 
+### C. Criação da GUI, aprimoramento do tracking e encapsulamento em funções
+
+Após os testes em A e B, iniciou-se a adaptação ao modo de GUI. O código parcial antes da implementação da GUI está disponível no arquivo parcialB.py.
+Utilizando de referência o vídeo tutorial [3] são inicializadas trackbars dentro de uma janela que recebe input do usuário, e é criada uma função normalizadora que garante que os valores de input sejam válidos, além disso, é introduzida a limiarização através da função threshold. 
+
+A função é aplicada com as flags BINARY e OTSU, binary se refere à simples ativação ou desativação do pixel na máscara se o valor estiver acima ou abaixo do limiar, já a flag OTSU aplica um algoritmo que encontra o melhor valor para essa limiarização. 
+Foi permitido que o usuário escolha se quer identificar ou não múltiplos objetos. (Anexos C6 e C7)
+
+Como foi permitida a escolha do númeor de objetos a identificar, o bloco de código responsável por encontrar e contornar os objetos foi encapsulado na função <contornador()> que filtra contornos relevantes, desenha um retângulo ao redor deles, e, veremos mais à frente, encontra e exibe seus centros e trajetórias.
+
+### D. Exibição da trajetória do Objeto
+Com o tracking e contorno já implementados, para exibir a trajetória foram consideradas duas formas:
+Através das funções de optical flow de OpenCV, com base no vídeo[4] disponibilizado ou através de uma forma mais rudimentar porém direta, que é encontrando os centros dos contornos e guardando o histórico das últimas 10 posições em intervalos iguais (a cada 10 frames, ou pouco mais de 10 milissegundos).
+
+O maior ponto negativo da primeira opção era a dificuldade de lidar com ruído na imagem, pois a implementação sugerida procura "cantos" na imagem, o que gerava múltiplas linhas de movimento para um só objeto, e apesar de ser uma melhor visualização de volume, gerava pontos desnecessários e era e alta complexidade. Por causa desse ruído, foi decidido seguir pela segunda opção.
+
+Pontos negativos da segunda opção incluem não exibir o movimento das bordas do objeto, mas sim de um ponto central, e também não ter uma visualização contínua da trajetória. Porém a visualização pode se tornar mais contínua se o intervalo de tempo entre registro de pontos for diminuído, e o tamanho do array de pontos for aumentado. A quantidade mantida em código foi arbitrada para cumprir o propósito de mostrar o movimento enquanto também economizando memória de processamento, evitando atrasos no vídeo. O modo de tracking de múltiplos objetos guarda menos pontos e os computa em intervalos maiores.
+
+A fórmula usada para calcular o centro do contorno[5] retorna um valor similar ao da diferença que poderíamos obter se procurássemos o centro do retângulo dividindo largura e altura por 2, porém ela considera o contorno, o que leva ao ponto nem sempre estar no centro exato do retângulo, mas ainda assim no centroide da máscara.
+
+
 
 --------
 BIBLIOGRAFIA:
 
 [1] Apostila - Introdução a Visão Computacional com Python e OpenCV (https://professor.luzerna.ifc.edu.br/ricardo-antonello/wp-content/uploads/sites/8/2017/02/Livro-Introdu%C3%A7%C3%A3o-a-Vis%C3%A3o-Computacional-com-Python-e-OpenCV-3.pdf)
 
-[2] Tutorial - Detect Color in OpenCV (https://www.youtube.com/watch?v=cMJwqxskyek)
+[2] How to Detect Colors in OpenCV [Python] (https://www.youtube.com/watch?v=cMJwqxskyek)
+
+[3] Tracking com Ajuste Dinâmico de Cores - Python & OpenCV 4 #08 (https://www.youtube.com/watch?v=NxyY3JBWoR4&t=30s&ab_channel=UniversoDiscreto)
+
+[4] From Beginner to Expert: Optical Flow for Object Tracking and Trajectories in OpenCV Python (https://www.youtube.com/watch?v=hfXMw2dQO4E&ab_channel=NicolaiNielsen-ComputerVision%26AI)
+
+[5] Python OpenCV – Find center of contour (https://www.geeksforgeeks.org/python-opencv-find-center-of-contour/#:~:text=To%20find%20the%20centroid%20of%20the%20image%2C%20we%20use%20the%20particular%20formula%3A)
--- SegundaEtapa/objectTracking.py
+++ SegundaEtapa/objectTracking.py
@@ -1,28 +1,20 @@
-import cv2
 import numpy as np
+import cv2
+import time
+
 
 # Determinar range de detecção no formato hsv 
 # hue - cor, 
 # saturation - intensidade, 
 # value - luminosidade
 
-# Através das trackbars, são determinados valores mínimos e máximos para os componentes de cor HSV.
-trackbars = "trackbars"
-cv2.namedWindow(trackbars) # Cria uma janela chamada trackbars, atribui-se seu nome à uma variável por conveniência
-
+### FUNÇÕES ###
 def onChange(val): # função de callback executada sempre que o valor muda
     pass
 
-
-def getContourCol(min, max):
-    hsvCol = [[[(min[0] + max[0]) / 2, (min[1] + max[1]) / 2, (min[2] + max[2]) / 2]]]
-    bgrCol = cv2.cvtColor(hsvCol, cv2.COLOR_HSV2BGR) 
-    return bgrCol
-
-
-
 # garante que o mínimo seja sempre menor que o máximo nas trackbars
 def normalizador():
+    # se a menor posição for igual à maior, a maior passa a "ser arrastada" junto à menor
     if cv2.getTrackbarPos("Min Hue", "trackbars") > cv2.getTrackbarPos("Max Hue", "trackbars"):
         cv2.setTrackbarPos("Max Hue", "trackbars", cv2.getTrackbarPos("Min Hue", "trackbars"))
 
@@ -32,7 +24,51 @@ def normalizador():
     if cv2.getTrackbarPos("Min Val", "trackbars") > cv2.getTrackbarPos("Max Val", "trackbars"):
         cv2.setTrackbarPos("Max Val", "trackbars", cv2.getTrackbarPos("Min Val", "trackbars"))
 
+# controla e desenha contornos, coordenadas, e trajetória na tela, 
+# periodo determina o intervalo entre capturas, inversamente proporcional à precisão
+# comprimento determina a quantidade de pontos que devem ser guardados, o tamanho do rastro
+def contornador(contorno, frame, contador, arrayPontos, periodo, comprimento):
 
+    # Filtragem para que apenas objetos de tamanho relevante sejam considerados
+    if cv2.contourArea(contorno) > 400:
+        # É encontrado o retângulo de menor área que pode ser desenhado a partir dos pontos do contorno, 
+        # boxPonts encontra seus vértices, gerando assim um retângulo possivelmente rotacionado, de menor área, que contém os pontos do contorno
+        rect = np.int32(cv2.boxPoints(cv2.minAreaRect(contorno)))
+
+        # moments retorna o momento da imagem, uma média de densidade de pixeis que ajuda a calcular o centroide do objeto
+        M = cv2.moments(contorno)
+        # fórmula para achar o centróide doobjeto baseado nos momentos e posições cx e cy (detalhes no item D)
+        if M['m00'] != 0:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+
+            # a cada <periodo> frames guardamos uma posição do centro do objeto
+            # sçao adicionados itens até que o tamanho do array -1 seja atingido
+            if contador % periodo == 0 and len(arrayPontos) <= comprimento:
+                arrayPontos.append((cx, cy))
+            # no segundo if desenhamos os últimos <comprimento> pontos, com o mais antigo sendo o mais esbranquiçado
+            if len(arrayPontos) >= 1:
+                for i in range(len(arrayPontos) -1):
+                    # subtraindo i * 11 ao valor de azul e verde, a cor fica mais escura a cada ponto
+                    cv2.circle(frame, arrayPontos[i], 4, (110 - i*11, 110 - i*11, 255), -1)
+                    # os pontos são movidos uma posição para trás como em um array, e o mais antigo é descartado
+                    arrayPontos[i] = arrayPontos[i+1]
+                    # é adicionado o ponto atual na última posição, a mais recente
+                    arrayPontos[-1] = (cx, cy)
+            # coordenadas do ponto atual, que serão impressas na tela
+            coordenada = f"x: {cx} y: {cy}"
+            # desenho do círculo atual é feito em todos os frames, não a cada dez, para explicitar a continuidade
+            cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
+            # texto indicando a posição atual do objeto é impresso na tela com um pequeno offset do ponto central
+            cv2.putText(frame, coordenada, (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        #retângulo que contém o objeto é desenhado na tela
+        cv2.drawContours(frame, [rect], -1, (0, 0, 255),2)
+
+#### EXECUÇÃO ###
+
+# Através das trackbars, são determinados valores mínimos e máximos para os componentes de cor HSV.
+trackbars = "trackbars"
+cv2.namedWindow(trackbars) # Cria uma janela chamada trackbars, atribui-se seu nome à uma variável por conveniência
 
 # Na janela trackbars, cria trackbars para definição dos valores 
 cv2.createTrackbar("Min Hue", trackbars, 0, 255, onChange)
@@ -41,9 +77,9 @@ cv2.createTrackbar("Min Sat", trackbars, 0, 255, onChange)
 cv2.createTrackbar("Max Sat", trackbars, 0, 255, onChange)
 cv2.createTrackbar("Min Val", trackbars, 0, 255, onChange)
 cv2.createTrackbar("Max Val", trackbars, 0, 255, onChange)
-
+# trackbar que funciona como switch e permite o usuário determinar o que quer computar
 cv2.createTrackbar("Detectar Múltiplos", trackbars, 0, 1, onChange)
-
+                
 
 # criamos um objeto VideoCapture que auxilia no recebimento do video
 # pode receber um endereço de memória para um vídeo ou o index
@@ -51,16 +87,21 @@ cv2.createTrackbar("Detectar Múltiplos", trackbars, 0, 1, onChange)
 # câmera conectada ao computador
 video = cv2.VideoCapture(0)
 
+# contador registra qual é o frame atual, para ser usado na trajetória, e pontos mantém as coordenadas relevantes para o mesmo propósito
+contador = 0
+pontos = []
+
 while True: 
+    contador += 1
 # OBTENÇÃO DO FRAME e CONVERSÂO
-    # Sucesso guarda um booleano que diz se foi possível ler o vídeo
+    # Sucesso guarda um booleano que diz se foi possível ler o vídeo, se falso, encerra o programa
     # img recebe o frame do video
-    sucesso, img = video.read()
+    sucesso, frame = video.read()
     if not sucesso:
         break
 
     # converte a imagem de RGB para HSV
-    cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 # OBTENÇÃO DOS VALORES DOS TRACKBARS    
     # recebe o valor de input das trackbars
@@ -78,11 +119,15 @@ while True:
 
 # MÀSCARA
     # máscara que mostra o que está sendo detectado (preto e branco), será passada como parâmetro para encontrar seu contorno
-    mask = cv2.inRange(img, minimo, maximo)
+    mask = cv2.inRange(frame, minimo, maximo)
     # máscara que permite exibir apenas os pixels que estão sendo capturados no range
-    mask = cv2.bitwise_and(img, img, mask = mask)
+    mask = cv2.bitwise_and(frame, frame, mask = mask)
     # converte imagem para preto e branco
+    # usamos a máscara pois queremos considerar apenas a cor sendo processada
     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY) 
+    # blur ajuda a remover ruído
+    mask = cv2.GaussianBlur(mask, (3, 3), 0)
+
     # aplica limiarização binary com o limiar definido por OTSU
     _,mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) 
 
@@ -92,28 +137,27 @@ while True:
     # Hierarquia se refere à topologia dos contornos, é retornada pela função findContours.
     contornos, hierarquia = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    # Filtragem para que apenas objetos de tamanho relevante sejam considerados
+# se houver algum contorno
     if len(contornos) > 0:
+        # checa quantos contornos deve acompanhar
         if cv2.getTrackbarPos("Detectar Múltiplos", trackbars) == 1: 
+            # roda o contornador para todos os contornos relevantes
             for contorno in contornos:
-                if cv2.contourArea(contorno) > 400:
-                    rect = np.int32(cv2.boxPoints(cv2.minAreaRect(contorno)))
-                    cv2.drawContours(img, [rect], -1, (168, 95, 66),2)
+                contornador(contorno, frame, contador, pontos, 10, 10)
         else:
+            # seleciona o maior contorno
             contorno = sorted(contornos, key = cv2.contourArea, reverse = True)[0]
-            if cv2.contourArea(contorno) > 400:
-                # É encontrado o retângulo de menor área que pode ser desenhado a partir dos pontos do contorno, 
-                # boxPonts encontra seus vértices, gerando assim um retângulo possivelmente rotacionado, de menor área, que contém os pontos do contorno
-                rect = np.int32(cv2.boxPoints(cv2.minAreaRect(contorno)))
-                cv2.drawContours(img, [rect], -1, (168, 95, 66),2)
+            # roda o contornador para todos os contornos relevantes
+            contornador(contorno, frame, contador, pontos, 5, 20)
+
 
 # EXIBIÇÂO
     # Exibem a imagem da webcam com os objetos destacados
-    cv2.imshow("webcam", img)
+    cv2.imshow("webcam", frame)
     # Exibe a máscara do que está sendo detectado
     cv2.imshow("mask", mask)
 
-    #aguarda 1 milissegundo antes de mostrar o pròximo frame e checa para ver se a tecla de saída foi pressionada para encerrar o loop
+    #aguarda 1 milissegundo antes de mostrar o pròximo frame ou checa para ver se a tecla de saída foi pressionada para encerrar o loop
     if cv2.waitKey(1) & 0xFF == ord("q") or 0xFF == 27:
         break
 # ENCERRAMENTO    
